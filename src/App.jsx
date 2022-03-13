2ca6acf48873b73de551193cb3a253712e736ba4
Leonid Kudryashov <leoural66@yandex.ru>
2022-03-13 20:19:32 +0500

    два reducer один подключен

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+
+function App() {
+
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash.cash)
+  console.log(cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: 'ADD_CASH', payload: cash })
+  }
+
+  const getCash = (cash) => {
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
+
+  return (
+    <div className="App">
+      <div style={{ display: 'flex' }}>
+        <div style={{ fontSize: '3rem', margin: '10px' }} > {cash} </div>
+        <button onClick={() => addCash(Number(prompt()))} > Пополнить счет </button>
+        <button onClick={() => getCash(Number(prompt()))} > Снять со счета </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
